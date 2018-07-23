942094638ac657d8dc3bb766732858aada0bfd91
yoichiro-uno <yoichiro.uno@gmail.com>
2018-07-23 20:28:36 +0900

    Added text to image 2

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -42,9 +42,8 @@ class FaceDetector(object):
 #            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
 
-
         font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img,'OpenCV',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
+        cv2.putText(frame,'OpenCV',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
 
         # frameを戻り値として返す
         return frame
